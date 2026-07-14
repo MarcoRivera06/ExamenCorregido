18c7677202a17b617fd0eab4603a4cf8f05b599a
Joel Jara <zahid.jara@epn.edu.ec>
2026-07-14 08:51:16 -0500

    Completando

--- src/GUI/Form/MainPanel.java
+++ src/GUI/Form/MainPanel.java
@@ -1,5 +1,16 @@
 package GUI.Form;
 
-public class MainPanel {
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+
+public class MainPanel extends JPanel{
+    public MainPanel(){
+        customizeComponent();
+    }
+    
 
 }
