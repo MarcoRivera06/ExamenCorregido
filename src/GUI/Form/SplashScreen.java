9e7cb3c5ed45308547076fafb9b6ee1d02bdffd6
Joel Jara <zahid.jara@epn.edu.ec>
2026-07-14 08:38:57 -0500

    Completando

---
+++ src/GUI/Form/SplashScreen.java
@@ -0,0 +1,15 @@
+package GUI.Form;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JProgressBar;
+
+public class SplashScreen {
+private static JFrame frmSaplash;
+private static JProgressBar prdbLoading;
+private static ImageIcon icoImagen;
+private static JLabel lblSplash;
+
+
+
+}
---
+++ src/GUI/Resource/Style.java
@@ -0,0 +1,9 @@
+package GUI.Resource;
+
+import java.net.URL;
+
+public class Style {
+
+    public static final URL URL_SPLASH= Style.class.getResource("/GUI/Resource/img/Homero.png");
+
+}
